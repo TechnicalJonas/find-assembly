9541dcbb8a113d8cb1178b0e6dba45e7679294bf
TechnicalJonas <93896120+TechnicalJonas@users.noreply.github.com>
2023-07-25 18:11:02 -0500

    Add files via upload

---
+++ locate assembly.py
@@ -0,0 +1,379 @@
+
+shelf1 = {
+  'row0' : {
+    'bin1': ['A1', 'A2', 'A3'],
+    'bin2': ['A4', 'A5', 'A6'],
+    'bin3': ['A7', 'A8', 'A9']
+  },
+  'row1' : {
+    'bin1': ['A10'],
+    'bin2': ['A11', 'A12', 'A13'],
+    'bin3': ['A14', 'A15']
+  },
+  'row2' : {
+    'bin1': ['A16', 'A17', 'A18'],
+    'bin2': ['A19'],
+    'bin3': ['A20', 'A21', 'A22']
+  },
+  'row3' : {
+    'bin1': ['A23'],
+    'bin2': ['A24'],
+    'bin3': ['A25']
+  },
+  'row4' : {
+    'bin1': ['A26'],
+    'bin2': ['A27', 'A28'],
+    'bin3': ['A29']
+  },
+  'row5' : {
+    'bin1': ['A30'],
+    'bin2': ['A31', 'A32', 'A33'],
+    'bin3': ['A34']
+  },
+  'row6' : {
+    'bin1': ['A35', 'A36', 'A37'],
+    'bin2': ['A38'],
+    'bin3': ['A39']
+  },
+  'row7' : {
+    'bin1': ['A40'],
+    'bin2': ['A41'],
+    'bin3': ['A42']
+  },
+  'row8' : {
+    'bin1': ['A43', 'A44'],
+    'bin2': ['A45', 'A46'],
+    'bin3': ['A47']
+  },
+}
+
+shelf2 = {
+  'row0' : {
+    'bin1': ['A48'],
+    'bin2': ['A49'],
+    'bin3': ['50']
+  },
+  'row1' : {
+    'bin1': ['A51'],
+    'bin2': ['A52'],
+    'bin3': ['A53', 'A54']
+  },
+  'row2' : {
+    'bin1': ['A55'],
+    'bin2': ['A56'],
+    'bin3': ['A57']
+  },
+  'row3' : {
+    'bin1': ['A58', 'A59'],
+    'bin2': ['A60'],
+    'bin3': ['A61', 'A62', 'A63']
+  },
+  'row4' : {
+    'bin1': ['A64'],
+    'bin2': ['A65'],
+    'bin3': ['A66']
+  },
+  'row5' : {
+    'bin1': ['A67'],
+    'bin2': ['A68'],
+    'bin3': ['A69']
+  },
+  'row6' : {
+    'bin1': ['A70'],
+    'bin2': ['A71'],
+    'bin3': ['A72']
+  },
+  'row7' : {
+    'bin1': ['A73'],
+    'bin2': ['A74'],
+    'bin3': ['A75', 'A76']
+  },
+  'row8' : {
+    'bin1': ['A77'],
+    'bin2': ['A78', 'A79'],
+    'bin3': ['A80']
+  },
+}
+
+shelf3 = {
+  'row0' : {
+    'bin1': ['A81'],
+    'bin2': ['A82', 'A83'],
+    'bin3': ['A84']
+  },
+  'row1' : {
+    'bin1': ['A85'],
+    'bin2': ['A86'],
+    'bin3': ['A87']
+  },
+  'row2' : {
+    'bin1': ['A90'],
+    'bin2': ['A91'],
+    'bin3': ['A92']
+  },
+  'row3' : {
+    'bin1': ['A93'],
+    'bin2': ['A94'],
+    'bin3': ['A95', 'A96']
+  },
+  'row4' : {
+    'bin1': ['A97'],
+    'bin2': ['A98'],
+    'bin3': ['A99']
+  },
+  'row5' : {
+    'bin1': ['A100'],
+    'bin2': ['A101'],
+    'bin3': ['A102']
+  },
+  'row6' : {
+    'bin1': ['A103'],
+    'bin2': ['A104'],
+    'bin3': ['A105', '106']
+  },
+  'row7' : {
+    'bin1': ['A107'],
+    'bin2': ['A108', 'A109'],
+    'bin3': ['A110']
+  },
+  'row8' : {
+    'bin1': ['A111', 'A112'],
+    'bin2': ['A113'],
+    'bin3': ['A114']
+  },
+}
+
+shelf4 = {
+  'row0' : {
+    'bin1': ['A115'],
+    'bin2': ['A116', 'A117'],
+    'bin3': ['A118']
+  },
+  'row1' : {
+    'bin1': ['A119'],
+    'bin2': ['A120'],
+    'bin3': ['A121']
+  },
+  'row2' : {
+    'bin1': ['A122'],
+    'bin2': ['A123'],
+    'bin3': ['A124']
+  },
+  'row3' : {
+    'bin1': ['A125', 'A126'],
+    'bin2': ['A127', 'A128'],
+    'bin3': ['A129']
+  },
+  'row4' : {
+    'bin1': ['A130'],
+    'bin2': ['A131'],
+    'bin3': ['A132']
+  },
+  'row5' : {
+    'bin1': ['A133'],
+    'bin2': ['A134'],
+    'bin3': ['A135']
+  },
+  'row6' : {
+    'bin1': ['A136'],
+    'bin2': ['A137'],
+    'bin3': ['A138']
+  },
+  'row7' : {
+    'bin1': ['A139'],
+    'bin2': ['A140'],
+    'bin3': ['A141']
+  },
+  'row8' : {
+    'bin1': ['A142'],
+    'bin2': ['A143'],
+    'bin3': ['A144']
+  },
+}
+
+shelf5 = {
+  'row0' : {
+    'bin1': ['A145', 'A146'],
+    'bin2': ['A147'],
+    'bin3': ['A148']
+  },
+  'row1' : {
+    'bin1': ['A149'],
+    'bin2': ['A150'],
+    'bin3': ['A151']
+  },
+  'row2' : {
+    'bin1': ['A152'],
+    'bin2': ['A153'],
+    'bin3': ['A154', 'A155']
+  },
+  'row3' : {
+    'bin1': ['A156'],
+    'bin2': ['A157'],
+    'bin3': ['A158', 'A159']
+  },
+  'row4' : {
+    'bin1': ['A160'],
+    'bin2': ['A161'],
+    'bin3': ['A162']
+  },
+  'row5' : {
+    'bin1': ['A163', 'A164'],
+    'bin2': ['A165', 'A166'],
+    'bin3': ['A167']
+  },
+  'row6' : {
+    'bin1': ['A168'],
+    'bin2': ['A169'],
+    'bin3': ['A170']
+  },
+  'row7' : {
+    'bin1': ['A171'],
+    'bin2': ['A172'],
+    'bin3': ['A173']
+  },
+  'row8' : {
+    'bin1': ['A174'],
+    'bin2': ['A175'],
+    'bin3': ['A176']
+  },
+}
+
+shelf6 = {
+  'row0' : {
+    'bin1': ['A177'],
+    'bin2': ['A178'],
+    'bin3': ['A179']
+  },
+  'row1' : {
+    'bin1': ['A180'],
+    'bin2': ['A181'],
+    'bin3': ['A182']
+  },
+  'row2' : {
+    'bin1': ['A183'],
+    'bin2': ['A184'],
+    'bin3': ['A185']
+  },
+  'row3' : {
+    'bin1': ['A186'],
+    'bin2': ['A187'],
+    'bin3': ['A188']
+  },
+  'row4' : {
+    'bin1': ['A189'],
+    'bin2': ['A190'],
+    'bin3': ['A191']
+  },
+  'row5' : {
+    'bin1': ['A192'],
+    'bin2': ['A193', 'A194'],
+    'bin3': ['A195']
+  },
+  'row6' : {
+    'bin1': ['A196'],
+    'bin2': ['A197'],
+    'bin3': ['A198']
+  },
+  'row7' : {
+    'bin1': ['A200'],
+    'bin2': ['A201'],
+    'bin3': ['A202']
+  },
+  'row8' : {
+    'bin1': ['A203', 'A204'],
+    'bin2': ['A205', 'A206'],
+    'bin3': ['A207']
+  },
+}
+
+shelf7 = {
+  'row0' : {
+    'bin1': {'A208'},
+    'bin2': {'A209'},
+    'bin3': {'A210'}
+  },
+  'row1' : {
+    'bin1': ['A211'],
+    'bin2': ['A212'],
+    'bin3': ['A213']
+  },
+  'row2' : {
+    'bin1': ['A214'],
+    'bin2': ['A215'],
+    'bin3': ['A216']
+  },
+  'row3' : {
+    'bin1': ['A217'],
+    'bin2': ['A218'],
+    'bin3': ['A219']
+  },
+  'row4' : {
+    'bin1': ['A220'],
+    'bin2': ['A221'],
+    'bin3': ['A222']
+  },
+  'row5' : {
+    'bin1': ['A223'],
+    'bin2': ['A224'],
+    'bin3': ['A225']
+  },
+  'row6' : {
+    'bin1': ['A226'],
+    'bin2': ['A227'],
+    'bin3': ['A228']
+  },
+  'row7' : {
+    'bin1': ['A229'],
+    'bin2': ['A229'],
+    'bin3': ['A230']
+  },
+  'row8' : {
+    'bin1': ['A231', 'A232'],
+    'bin2': ['A233'],
+    'bin3': ['A234']
+  },
+}
+
+
+testShelf = [shelf1, shelf2, shelf3, shelf4, shelf5, shelf6, shelf7]
+strShelf = ['shelf1','shelf2','shelf3','shelf4','shelf5','shelf6','shelf7']
+
+while True:
+  print('what board are you looking for? ')
+  print('To leave, press ctrl+c')
+  assm = input('--> ').upper()
+  print(assm)
+  shlf = None
+  row = None
+  bin = None
+
+  for shelf in testShelf:
+    shlfNum = testShelf.index(shelf)
+    for i, j in shelf.items():
+      temp1 = i
+      for k,l in j.items():
+        temp2 = k
+        if assm in l:
+          shlf = strShelf[shlfNum]
+          row = temp1
+          bin = temp2
+          break
+      if shlf is not None:
+        break
+    if shlf is not None:
+      break
+      
+  try:
+    if shlf is None or row is None or bin is None:
+      print('item does not exist')
+      print('')
+    else:
+      print(shlf, row, bin)
+      print('')
+  except NameError:
+    print('invalid board assembly. Please try again')
+    print('')
+
+
+  
